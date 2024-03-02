7f1e02f20791369c39027538eecddd213e80f0b4
Shuto Tamaoka <shuto@shutotamaoka.xyz>
2024-03-02 18:15:38 +0900

    update: some modifications
    - set humble as default ROS distro
    - add colcon completions
    - use direnv to source files

--- setup.bash
+++ setup.bash
@@ -1,33 +1,41 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
 #SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
 #SPDX-License-Identifier: BSD-3-Clause
 
-[ "${ROS_VERSION}" = "1" ] && echo ROS1 is set. Please comment out the lines of source for ROS1 on .bashrc and restart a terminal. && exit 1
-[ "${ROS_VERSION}" = "2" ] && echo ROS2 is set. Please comment out the lines of source for ROS2 on .bashrc and restart a terminal. && exit 1
-
-sudo apt-get update
-sudo apt-get install -y curl gnupg2
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-
 UBUNTU_VER=$(lsb_release -sc)
-ROS_VER=dashing
-[ "$UBUNTU_VER" = "focal" ] && ROS_VER=foxy
-[ "$UBUNTU_VER" = "jammy" ] && ROS_VER=humble
+ROS_VER=humble
+
+[ "$UBUNTU_VER" != "jammy" ] &&
+    { echo "Please use Ubuntu 22.04 to run this script. Aborting..."; exit 1; }
+
+apt list --installed 2>/dev/null | grep -q "ros-${ROS_VER}" &&
+    { echo "ROS packages are already installed. Aborting..."; exit 1; }
+
+sudo apt update
+sudo apt install -y curl gnupg2
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 echo "deb [ signed-by=/usr/share/keyrings/ros-archive-keyring.gpg arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu ${UBUNTU_VER} main" |
 sudo tee /etc/apt/sources.list.d/ros2-latest.list
 
 #sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F42ED6FBAB17C654
-sudo apt-get update
-sudo apt-get install -y ros-${ROS_VER}-desktop python3-colcon-common-extensions python3-rosdep python3-argcomplete 
+sudo apt update
+sudo apt install -y ros-${ROS_VER}-desktop python3-colcon-common-extensions python3-rosdep python3-argcomplete
 
 sudo rm -f /etc/ros/rosdep/sources.list.d/20-default.list
 sudo rosdep init
 sudo rosdep fix-permissions
 
-echo "source /opt/ros/${ROS_VER}/setup.bash" >> ~/.bashrc
+# echo "source /opt/ros/${ROS_VER}/setup.bash" >> ~/.bashrc
 
 rosdep update
+
+sudo apt install -y direnv
+
+printf 'source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+source /usr/share/colcon_cd/function/colcon_cd.sh
+eval "$(direnv hook bash)"\n' >> ~/.bashrc
 
 echo '***INSTRUCTION*****************'
 echo '* do the following command    *'
